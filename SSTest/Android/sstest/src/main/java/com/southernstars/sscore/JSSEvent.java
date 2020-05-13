986afd34c47f76c6a81a8c7e08ef53e01c2c4d4f
Author: webadmin <setokt@gmail.com>
2020-05-13 12:52:50 -0700

    Initial JSSEvent

---
+++ SSTest/Android/sstest/src/main/java/com/southernstars/sscore/JSSEvent.java
@@ -0,0 +1,88 @@
+package com.southernstars.sscore;
+
+import java.util.ArrayList;
+import com.southernstars.sscore.JSSAngle;
+
+// This class computes times and circumstances of astronomical events like
+// object rising/transit/setting, satellite passes, moon phases, conjuctions,
+// oppositions, etc.
+
+public class JSSRTS
+{
+    public JSSTime time;
+    public JSSAngle azm;
+    public JSSAngle alt;
+}
+
+public class JSSPass
+{
+    public JSSRTS rising;
+    public JSSRTS transit;
+    public JSSRTS setting;
+}
+
+public class JSSEventTime
+{
+    public JSSTime time;
+    public double value;
+}
+
+// No need convert this coz it's not public?  typedef double (*SSEventFunc) ( SSCoordinates &coords, SSObjectPtr pObj1, SSObjectPtr pObj2 );
+
+public class JSSEvent
+{   // Lines copied over from from JSSCoordinates.java....
+    //
+    // private long pCoords;   // pointer to native C++ SSObject    <-- ... what does this do?
+
+    // private JSSCoordinates()     <-- related to the pointer above
+    // {
+    //     pCoords = 0;
+    // }
+
+    // public void finalize()       <-- what does this do?
+    // {
+    //     destroy();
+    // }
+
+    // public JSSEvent ( JSSTime time, JSSSpherical loc )
+    // {
+    //     create ( time, loc );
+    // }
+
+    // public native void create ( JSSTime time, JSSSpherical loc );
+    // public native void destroy();
+
+    public static final int kRise = -1;        // event sign for rising, to be used with riseTransSet, etc().
+    public static final int kTransit = 0;      // event sign for transit, to be used with riseTransSet, etc().
+    public static final int kSet = 1;          // event sign for setting, to be used with riseTransSet, etc().
+
+    public static final double kDefaultRiseSetAlt = -30.0 / JSSAngle.kArcminPerRad;        // geometric altitude of point object when rising/setting [radians]
+    public static final double kSunMoonRiseSetAlt = -50.0 / JSSAngle.kArcminPerRad;        // geometric altitude of Sun/Moon's apparent disk center when rising/setting [radians]
+    public static final double kSunCivilDawnDuskAlt = -6.0 / JSSAngle.kDegPerRad;          // geometric altitude of Sun's apparent disk center at civil dawn/dusk [radians]
+    public static final double kSunNauticalDawnDuskAlt = -12.0 / JSSAngle.kDegPerRad;      // geometric altitude of Sun's apparent disk center at nautical dawn/dusk [radians]
+    public static final double kSunAstronomicalDawnDuskAlt = -18.0 / JSSAngle.kDegPerRad;  // geometric altitude of Sun's apparent disk center at astronomical dawn/dusk [radians]
+
+    public static final double kNewMoon = 0.0;
+    public static final double kFirstQuarterMoon = JSSAngle.kHalfPi;
+    public static final double kFullMoon = JSSAngle.kPi;
+    public static final double kLastQuarterMoon = 3.0 * JSSAngle.kHalfPi;
+
+    public static native JSSAngle semiDiurnalArc ( JSSAngle lat, JSSAngle dec, JSSAngle alt );
+    
+    public static native JSSTime riseTransitSet ( JSSTime jd, JSSAngle ra, JSSAngle dec, int sign, JSSAngle lon, JSSAngle lat, JSSAngle alt );
+    public static native JSSTime riseTransitSet ( JSSTime time, JSSCoordinates coords, JSSObjectPtr pObj, int sign, JSSAngle alt );
+    public static native JSSTime riseTransitSetSearch ( JSSTime time, JSSCoordinates coords, JSSObjectPtr pObj, int sign, JSSAngle alt );
+    public static native JSSTime riseTransitSetSearchDay ( JSSTime today, JSSCoordinates coords, JSSObjectPtr pObj, int sign, JSSAngle alt );
+
+    public static native JSSPass riseTransitSet ( JSSTime today, JSSCoordinates coords, JSSObjectPtr pObj, JSSAngle alt );
+    public static native int findSatellitePaJsses ( JSSCoordinates coords, JSSObjectPtr pSat, JSSTime start, JSSTime stop, double minAlt, ArrayList<JSSPass> passes, int maxPassses );
+
+    public static native JSSTime nextMoonPhase ( JSSTime time, JSSObjectPtr pSun, JSSObjectPtr pMoon, double phase );
+    
+    public static native void findEvents ( JSSCoordinates coords, JSSObjectPtr pObj1, JSSObjectPtr pObj2, JSSTime start, JSSTime stop, double step, bool max, double limit, JSSEventFunc func, ArrayList<JSSEventTime> events, int maxEvents );
+    public static native void findEqualityEvents ( JSSCoordinates coords, JSSObjectPtr pObj1, JSSObjectPtr pObj2, JSSTime start, JSSTime stop, double step, bool max, double value, JSSEventFunc func, ArrayList<JSSEventTime> events, int maxEvents );
+    public static native void findConjunctions ( JSSCoordinates coords, JSSObjectPtr pObj1, JSSObjectPtr pObj2, JSSTime start, JSSTime stop, ArrayList<JSSEventTime> events, int maxEvents );
+    public static native void findOppositions ( JSSCoordinates coords, JSSObjectPtr pObj1, JSSObjectPtr pObj2, JSSTime start, JSSTime stop, ArrayList<JSSEventTime> events, int maxEvents );
+    public static native void findNearestDistances ( JSSCoordinates coords, JSSObjectPtr pObj1, JSSObjectPtr pObj2, JSSTime start, JSSTime stop, ArrayList<JSSEventTime> events, int maxEvents );
+    public static native void findFarthestDistances ( JSSCoordinates coords, JSSObjectPtr pObj1, JSSObjectPtr pObj2, JSSTime start, JSSTime stop, ArrayList<JSSEventTime> events, int maxEvents );
+}
