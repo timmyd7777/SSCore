06b5ce6f63a9ff8495eead769407a40b58ff2b48
ultrapre <magsec2@gmail.com>
2020-11-13 02:57:57 +0800

    build qt proj

---
+++ SSTest/QtProjects/mainwindow.h
@@ -0,0 +1,89 @@
+﻿#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+#include <QDebug>
+//  SSTest.cpp
+//  SSCore
+//
+//  Created by Tim DeBenedictis on 2/24/20.
+//  Copyright © 2020 Southern Stars. All rights reserved.
+
+#include <cstdio>
+#include <iostream>
+
+#if defined __APPLE__
+#include <TargetConditionals.h>
+#endif
+
+#ifdef _WIN32
+#include <Windows.h>
+#endif
+
+#include "SSCoordinates.hpp"
+#include "SSOrbit.hpp"
+#include "SSPlanet.hpp"
+#include "SSStar.hpp"
+#include "SSConstellation.hpp"
+#include "SSImportHIP.hpp"
+#include "SSImportSKY2000.hpp"
+#include "SSImportNGCIC.hpp"
+#include "SSImportMPC.hpp"
+#include "SSImportGJ.hpp"
+#include "SSJPLDEphemeris.hpp"
+#include "SSTLE.hpp"
+#include "SSEvent.hpp"
+#include "VSOP2013.hpp"
+#include "ELPMPP02.hpp"
+
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+//public slots:
+//    void TestDeepSkyNew ();
+
+//signals:
+////    // 信号与槽关联
+//    void sigtest();
+
+private slots:
+    void on_pushButton_clicked();
+
+    void on_pushButton_2_clicked();
+
+    void on_pushButton_3_clicked();
+
+    void on_pushButton_4_clicked();
+
+    void on_pushButton_5_clicked();
+
+    void on_pushButton_6_clicked();
+
+    void on_pushButton_7_clicked();
+
+    void on_pushButton_8_clicked();
+
+    void on_pushButton_9_clicked();
+
+    void on_pushButton_10_clicked();
+
+    void on_pushButton_11_clicked();
+
+    void on_pushButton_12_clicked();
+
+    void on_pushButton_13_clicked();
+
+private:
+    Ui::MainWindow *ui;
+};
+#endif // MAINWINDOW_H
