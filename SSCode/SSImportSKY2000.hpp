11dc7deaeaf675da226b1e2c431681a5e9256d15
Tim DeBenedictis <timd@southernstars.com>
2021-04-25 12:55:38 -0700

    Fix broken SSImportSKY2000 prototype.

--- SSCode/SSImportSKY2000.hpp
+++ SSCode/SSImportSKY2000.hpp
@@ -16,6 +16,6 @@
 #include "SSStar.hpp"
 
 int SSImportIAUStarNames ( const string &filename, SSIdentifierNameMap &nameMap );
-int SSImportSKY2000 ( const string &filename, SSIdentifierNameMap &nameMap, SSObjectVec &hipStars, SSObjectVec &gjStars, SSObjectVec &stars, SSObjectFilter filter, void *userData );
+int SSImportSKY2000 ( const string &filename, SSIdentifierNameMap &nameMap, SSObjectVec &hipStars, SSObjectVec &gjStars, SSObjectVec &stars, SSObjectFilter filter = nullptr, void *userData = nullptr );
 
 #endif /* SSImportSKY2000_hpp */
