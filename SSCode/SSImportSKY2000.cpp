80183070d6001c55e8f1889dc927aaaafb48b9c2
Tim DeBenedictis <timd@southernstars.com>
2021-05-05 08:39:00 -0700

    Rework code which adds missing HR numbers.

--- SSCode/SSImportSKY2000.cpp
+++ SSCode/SSImportSKY2000.cpp
@@ -384,6 +384,18 @@ int SSImportSKY2000 ( const string &filename, SSIdentifierNameMap &nameMap, SSOb
         if ( ! strWDS.empty() )
             SSAddIdentifier ( SSIdentifier::fromString ( "WDS " + strWDS ), idents );
         
+        // SKY2000 is missing four HR stars; all of them are double star secondary components.
+        // This adds those missing HR identifiers to their four SKY2000 primary stars.
+
+        if ( ! strHR.empty() )
+        {
+            static map<int,int> missingHR = // map from SKY2000 HR to missing HR
+            { { 927, 928 }, { 4375, 4374 }, { 4968, 4969 }, { 5978, 5977 } };
+            int hr = missingHR[ strtoint ( strHR ) ];
+            if ( hr )
+                SSAddIdentifier ( SSIdentifier ( kCatHR, hr ), idents );
+        }
+
         // Look for a GCVS star with the same HD number as our SKY2000 star.
         // If we find one, add the GCVS star identifier to the SKY2000 star identifiers.
         // Otherwise, if we don't have any GCVS stars, use the GCVS identifier string from SKY2000.
@@ -502,27 +514,4 @@ int SSImportSKY2000 ( const string &filename, SSIdentifierNameMap &nameMap, SSOb
     // Return imported star count; file is closed automatically.
     
     return numStars;
-}
-
-// SKY2000 is missing four HR stars, all double star secondary components.
-// This function adds those missing HR identifiers to their four SKY2000 primary stars.
-// Returns the number of HR identifiers added (which should be 4!)
-
-int SSAddSKY2000MissingHRNumbers ( SSObjectVec &stars )
-{
-    int n = 0;
-
-    static map<int64_t,int64_t> sky2000missingHR = // map from SKY2000 HR to missing HR
-    { { 927, 928 }, { 4375, 4374 }, { 4968, 4969 }, { 5978, 5977 } };
-    
-    for ( int i = 0; i < stars.size(); i++ )
-    {
-        SSStar *pStar = SSGetStarPtr ( stars.get ( i ) );
-        int64_t hr = pStar->getIdentifier ( kCatHR ).identifier();
-        hr = sky2000missingHR[hr];
-        if ( hr )
-            n += pStar->addIdentifier ( SSIdentifier ( kCatHR, hr ) );
-    }
-    
-    return n;
 }
